f3b6e95a71ce4dfdbc001db34b04585775679360
Pablo Diaz <pablo.diaz.titelman@gmail.com>
2024-08-18 11:15:03 +0200

    feat: Added emoji management and title edit on page

--- convex/documents.ts
+++ convex/documents.ts
@@ -293,3 +293,28 @@ export const update = mutation ({
     return document;
   }
 })
+
+export const removeIcon = mutation({
+  args: {id: v.id("documents")},
+  handler: async (ctx, args) => {
+    const identity = await requiresAuth(ctx);
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      icon: undefined
+    });
+
+    return document;
+  }
+});
