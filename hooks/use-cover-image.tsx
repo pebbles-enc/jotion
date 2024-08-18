62a9a1ee3653c1bc99b36efbb768bd09ff0fa54a
Pablo Diaz <pablo.diaz.titelman@gmail.com>
2024-08-18 16:04:03 +0200

    fix: Remove comma that blocked deploy

--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -12,6 +12,6 @@ export const useCoverImage = create<CoverImageStore>((set) => ({
   url: undefined,
   isOpen: false,
   onOpen: () => set({ isOpen: true, url: undefined}),
-  onClose: () => set({ isOpen: false, , url: undefined}),
+  onClose: () => set({ isOpen: false, url: undefined}),
   onReplace: (url: string) => set({ isOpen: true, url})
 }))
