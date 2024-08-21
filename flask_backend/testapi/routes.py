059ba26471ecedd57a9087fff69bc32f4cf341b1
Pablo Diaz <pablo.diaz.titelman@gmail.com>
2024-08-21 12:04:16 +0200

    feat: Added models.py and db-status-check

--- flask_backend/testapi/routes.py
+++ flask_backend/testapi/routes.py
@@ -6,14 +6,15 @@ from flask import (
     request,
     redirect,
     url_for)
-# from flask_backend.models import HelloModel
+from flask_backend.models import HelloModel
 testapi = Blueprint("test", __name__)
 
 @testapi.route("/hello-world")
 def hello_world():
     return "<p>Hello, World zorrillooo asdas!</p>"
 
-# @testapi.route("/db-read-check")
-# def db_read_check():
-#     first_row = HelloModel.get_one_row(1)
-#     return f"{first_row.id, first_row.is_readable}", 200
+@testapi.route("/db-read-check")
+def db_read_check():
+    first_row = HelloModel.get_one_row(1)
+
+    return f"{first_row.id, first_row.is_readable}", 200
